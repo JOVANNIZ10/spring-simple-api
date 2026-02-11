0b4b5cc0bc317c85e287464c2059ce44d44fde37
JOVANNIZ10 <ipriala37@gmail.com>
2026-02-12 00:24:37 +0100

    Initial commit

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,11 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    
+}
