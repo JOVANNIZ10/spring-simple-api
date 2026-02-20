58b15966d995d289c46d3634cc3a5bc14734dff0
JOVANNIZ10 <ipriala37@gmail.com>
2026-02-20 08:49:52 +0100

    feat: BookRepository.java class

---
+++ src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.Book;
+
+public interface BookRepository extends JpaRepository <Book, UUID>  {
+
+    Optional <Book> findAllUserBooks (UUID userId); 
+}
